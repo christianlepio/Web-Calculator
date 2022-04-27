b991de643c5041e8fb882560d2471f73c5609923
babaGie <rylepio79@gmail.com>
2022-04-27 19:36:34 +0800

    update dark mode

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function darkMode(){
     var element = document.body
-    element.classList.toggle("dark-mode")
+    element.classList.add("dark-mode")
+    document.getElementById("night").style.display="none"
+    document.getElementById("sun").style.display="block"
+}
+function lightMode(){
+    var element = document.body
+    element.classList.remove("dark-mode")
+    document.getElementById("sun").style.display="none"
+    document.getElementById("night").style.display="block"
 }
