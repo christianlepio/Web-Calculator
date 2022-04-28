a4db9f12898be63888ec71b15879fb27c3958215
babaGie <rylepio79@gmail.com>
2022-04-28 16:46:48 +0800

    update ecma 6 syntax

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ del = () => {
     text="";
 }
 
-function darkMode(){
+darkMode = () =>{
     var element = document.body
     element.classList.add("dark-mode")
     document.getElementById("night").style.display="none"
     document.getElementById("sun").style.display="block"
 }
-function lightMode(){
+lightMode = () =>{
     var element = document.body
     element.classList.remove("dark-mode")
     document.getElementById("sun").style.display="none"
