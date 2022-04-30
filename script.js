a28f0a24a4edc7932b70354c5a3e9b5f257e3ca3
babaGie <rylepio79@gmail.com>
2022-04-30 16:57:42 +0800

    add ac bg color

--- script.js
+++ script.js
@@ -25,9 +25,11 @@ del = () => {
 
 var element = document.body
 var tblelement = document.getElementById("tbldark")
+var ac = document.querySelector(".ac")
 darkMode = () =>{
     element.classList.add("dark-mode")
     tblelement.classList.add("darktbl")
+    ac.classList.add("acdark")
 
     document.getElementById("night").style.display="none"
     document.getElementById("sun").style.display="block"
@@ -35,6 +37,7 @@ darkMode = () =>{
 lightMode = () =>{
     element.classList.remove("dark-mode")
     tblelement.classList.remove("darktbl")
+    ac.classList.remove("acdark")
     document.getElementById("sun").style.display="none"
     document.getElementById("night").style.display="block"
 }
