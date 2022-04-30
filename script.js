fe533e9565aa47fe7565c68ddedb8f0ada35c275
babaGie <rylepio79@gmail.com>
2022-04-30 17:17:21 +0800

    add bg-color to percent

--- script.js
+++ script.js
@@ -27,11 +27,14 @@ var element = document.body
 var tblelement = document.getElementById("tbldark")
 var ac = document.querySelector(".ac")
 var del = document.querySelector('.del')
+var perc = document.querySelector('.perc')
+
 darkMode = () =>{
     element.classList.add("dark-mode")
     tblelement.classList.add("darktbl")
     ac.classList.add("acdark")
     del.classList.add("deldark")
+    perc.classList.add("percdark")
 
     document.getElementById("night").style.display="none"
     document.getElementById("sun").style.display="block"
@@ -41,6 +44,7 @@ lightMode = () =>{
     tblelement.classList.remove("darktbl")
     ac.classList.remove("acdark")
     del.classList.remove("deldark")
+    perc.classList.remove("percdark")
 
     document.getElementById("sun").style.display="none"
     document.getElementById("night").style.display="block"
